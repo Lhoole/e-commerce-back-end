168a021e28a4156168caf133ccf1b5a742418751
Luke <116241757+Nyrexis@users.noreply.github.com>
2023-02-23 21:49:28 +1100

    added readme with video link

--- server.js
+++ server.js
@@ -16,6 +16,3 @@ sequelize.sync().then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
 
-// app.listen(PORT, () => {
-//   console.log(`App listening on port ${PORT}!`);
-// });
